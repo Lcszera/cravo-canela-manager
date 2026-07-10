6cb0d599e40b98c066a274a18b68c45f4c480f84
LUCAS MENEZES SILVA <lucasmen2005@gmail.com>
2026-07-10 14:27:44 -0300

    feat: implementa estrutura inicial do acesso ao banco de dados

--- src/br/com/cravoecanela/Main.java
+++ src/br/com/cravoecanela/Main.java
@@ -1,8 +1,8 @@
 package br.com.cravoecanela;
 
+import br.com.cravoecanela.entities.Produto;
 import br.com.cravoecanela.config.DatabaseInitializer;
 import br.com.cravoecanela.dao.ProdutoDAO;
-import br.com.cravoecanela.entities.Produto;
 import br.com.cravoecanela.enums.TipoProduto;
 
 public class Main {
@@ -12,6 +12,36 @@ public class Main {
         DatabaseInitializer.initializeDatabase();
 
         System.out.println("Sistema iniciado com sucesso!");
+
+        ProdutoDAO dao = new ProdutoDAO();
+
+        Produto produto = new Produto();
+
+        System.out.println("Quantidade de produtos: " + dao.listarTodos().size());
+
+        produto.setNome("Chá Verde");
+        produto.setDescricao("Auxilia no emagrecimento");
+        produto.setTipo(TipoProduto.CHA);
+        produto.setValor(15.90);
+        produto.setQuantidade(20);
+        produto.setEstoqueMinimo(5);
+        produto.setPrateleira(1);
+        produto.setColuna(2);
+        produto.setPalavrasChave("emagrecer, detox");
+
+        dao.salvar(produto);
+
+        System.out.println("Produtos cadastrados:");
+
+        for (Produto p : dao.listarTodos()) {
+
+            System.out.println(
+                    p.getId() + " - " +
+                            p.getNome() + " - " +
+                            p.getQuantidade()
+            );
+
+        }
 
     }
 
--- src/br/com/cravoecanela/dao/ProdutoDAO.java
+++ src/br/com/cravoecanela/dao/ProdutoDAO.java
@@ -2,7 +2,10 @@ package br.com.cravoecanela.dao;
 
 import br.com.cravoecanela.config.ConnectionFactory;
 import br.com.cravoecanela.entities.Produto;
-
+import br.com.cravoecanela.enums.TipoProduto;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -45,6 +48,8 @@ public class ProdutoDAO {
 
     }
 
+
+
     public void atualizar(Produto produto) {
 
     }
@@ -61,7 +66,42 @@ public class ProdutoDAO {
 
     public List<Produto> listarTodos() {
 
-        return null;
+        List<Produto> produtos = new ArrayList<>();
+
+        String sql = "SELECT * FROM produtos";
+
+        try (
+                Connection conn = ConnectionFactory.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)
+        ) {
+
+            while (rs.next()) {
+
+                Produto produto = new Produto();
+
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setDescricao(rs.getString("descricao"));
+                produto.setTipo(TipoProduto.valueOf(rs.getString("tipo")));
+                produto.setValor(rs.getDouble("valor"));
+                produto.setQuantidade(rs.getInt("quantidade"));
+                produto.setEstoqueMinimo(rs.getInt("estoque_minimo"));
+                produto.setPrateleira(rs.getInt("prateleira"));
+                produto.setColuna(rs.getInt("coluna"));
+                produto.setPalavrasChave(rs.getString("palavras_chave"));
+
+                produtos.add(produto);
+
+            }
+
+        } catch (Exception e) {
+
+            throw new RuntimeException("Erro ao listar produtos.", e);
+
+        }
+
+        return produtos;
 
     }
 
